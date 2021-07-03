ec67b5e47f8e7a20a4f848c99dc607ff4227064b
damigege <1654222066@qq.com>
2021-07-03 17:38:58 +0800

    完成了后台首页的功能

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,6 +3,13 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options) {
     // 在发起真正的 Ajax 请求之前，统一拼接请求的根路径
-    options.url = 'http://api-breakingnews-web.itheima.net' + options.url
+    options.url = 'http://api-breakingnews-web.itheima.net' + options.url;
+    // 在此处统一设置请求头，避免了在ajax请求中去反复的设置请求头参数
+    if (options.url.indexOf("/my/") !== -1) {
+        options.headers = {
+            Authorization: sessionStorage.getItem("token")
+        }
+    }
+
 
 })
